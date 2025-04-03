020bf2242f9c3196fe5f1ea582608c8748fda5fc
Rikuliini1 <rento.fox@gmail.com>
2025-04-03 14:41:53 +0300

    -

---
+++ React/React Tutorial/React 04 ES6/React 04.8 ES6 Modules/message.js
@@ -0,0 +1,11 @@
+// Default Exports
+    // Let us create another file, named message.js, and use it for demonstrating default export.
+    // You can only have one default export in a file.
+        const messageFromFile = () => {
+            const name = "Jesse";
+            const age = 27;
+            return name + ' is ' + age + ' years old.';
+        };
+        export default messageFromFile;
+
+// EOF
---
+++ React/React Tutorial/React 04 ES6/React 04.8 ES6 Modules/person.js
@@ -0,0 +1,12 @@
+// Named Exports
+    // You can create named exports two ways. In-line individually, or all at once at the bottom.
+    // In-line individually:
+        export const firstName = "Jesse"
+        export const intAge = 27
+
+    // All at once at the bottom:
+        const firstName2 = "Walter"
+        const intAge2 = 44
+        export { firstName2, intAge2 }
+
+// EOF
