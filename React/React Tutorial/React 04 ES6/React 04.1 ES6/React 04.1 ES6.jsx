f43182601556e0355b8f6d2d7e360153ec3ea866
Rikuliini1 <rento.fox@gmail.com>
2025-04-03 15:02:32 +0300

    -

--- React/React Tutorial/React 04 ES6/React 04.1 ES6/React 04.1 ES6.jsx
+++ React/React Tutorial/React 04 ES6/React 04.1 ES6/React 04.1 ES6.jsx
@@ -1,4 +1,4 @@
-// What is ES6?
+// React ES6
     // ES6 stands for ECMAScript 6.
     // ECMAScript was created to standardize JavaScript, and ES6 is the 6th version of ECMAScript.
     // It was published in 2015, and is also known as ECMAScript 2015.
