ecd0f49873cc33135ddafbda2eb6adbfa3d8bfec
Rikuliini1 <rento.fox@gmail.com>
2025-04-05 02:58:32 +0300

    -

---
+++ React/React Tutorial/React 17 Sass Styling/react-vite-js-app/src/App.jsx
@@ -0,0 +1,17 @@
+// Import the Sass file the same way as you imported a CSS file:
+
+import React from 'react';
+import './my-sass.scss';
+
+const Header = () => {
+    return (
+        <>
+        <h1>Hello Style!</h1>
+        <p>Add a little style!.</p>
+        </>
+    );
+}
+
+export default Header
+
+// EOF
---
+++ React/React Tutorial/React 17 Sass Styling/react-vite-js-app/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Header from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <Header />
+    </StrictMode>,
+)
+
+// EOF
